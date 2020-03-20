3cc12283f607ec6bd7e3d173f6eae11795d159e2
Clovis Daniel <clovisdanielcosta@gmail.com>
2020-03-20 01:57:39 -0300

    Ajustes no Ajax Hide/Show Sections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
+//Função para exibir as seções
+function hideAllSections() {
+  
+  $('section').hide();
+  $("#a_home").show();
+  $("#a_plataformas").show();
+}
+
+function showSection(a_section) {
+
+  switch (a_section) {
+      case 1:
+        a_section = "#a_home";
+          break;
+      case 2:
+        a_section = "#a_linux";
+          break;
+      case 3:
+        a_section = "#a_git";
+          break;
+      case 4:
+        a_section = "#a_vscode";
+          break;
+      case 5:
+        a_section = "#a_heroku";
+          break;
+      case 6:
+        a_section = "#a_html5";
+          break;
+      case 7:
+        a_section = "#a_javascript";
+          break;
+      case 8:
+        a_section = "#a_css3";
+          break;
+      case 9:
+        a_section = "#a_es6";
+          break;
+      case 10:
+        a_section = "#a_react";
+          break;
+      default:
+        a_section = "#a_home";
+          break;
+  }
+
+//  if (a_section === 2) {
+  //  a_section = "#a_linux";
+  //}
+    $('section').hide();
+    $(a_section).show();
+
+}
+
+
 // Função para recolher o menu quando a opção for selecionada
 $(document).ready(function () {
     $(".navbar-nav li a").click(function(event) {
