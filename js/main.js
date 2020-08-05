a68b151a7777e22d50b53497c5dc289e5df9d252
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-08-05 15:37:50 -0300

    Update main.js
    Add funcion to open in fullscreen

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ function hideAllSections() {
   $("#a_plataformas").show();
 }
 
+var elem = document.getElementById("myBody");
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+
 function showSection(a_section) {
 
   switch (a_section) {
